7901d3995fad9681b0e31d1dde586107904ce1fe
GunnerBel <m.bela@alustudent.com>
2025-01-06 09:45:47 +0200

    changed the value of love

--- 8-KYU/Opposites_Attract.js
+++ 8-KYU/Opposites_Attract.js
@@ -1,6 +1,6 @@
 function lovefunc(flower1, flower2){
 
-    let love = flower1 + flower2 % 2 === 0 ? false : true
+    let love = (flower1 + flower2) % 2 === 0 ? false : true
     return love
   
   }
